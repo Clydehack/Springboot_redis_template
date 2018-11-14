858b9691b24232ae48069c6cdcbdad691c12c9e7
zhaodanqing <zhaodanqing@sinosoft.com.cn>
2018-11-14 19:52:40 +0800

    今天有点烦，不撸了

---
+++ src/main/java/com/template/ie/redis/configuration/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.template.ie.redis.configuration;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	/* 缓存管理器 */
+	@Bean
+	public CacheManager cacheManager(RedisTemplate<?,?> redisTemplate) {
+		CacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		return cacheManager;
+	}
+
+	/* 应该是类似于mongodbTemplate一样的 */
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(factory);
+		return template;
+	}
+
+	/* 默认string的序列化方式，用于存储string格式，只支持KV为String的操作，具体待测试 */
+	@Bean
+	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+		stringRedisTemplate.setConnectionFactory(factory);
+		return stringRedisTemplate;
+	}
+}
