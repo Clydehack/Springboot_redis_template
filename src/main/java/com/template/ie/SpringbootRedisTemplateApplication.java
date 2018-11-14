794e6043542b16deb619ac70f34634098356a7e9
zhaodanqing <zhaodanqing@sinosoft.com.cn>
2018-11-14 19:27:21 +0800

    demo1

---
+++ src/main/java/com/template/ie/SpringbootRedisTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.template.ie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisTemplateApplication.class, args);
+	}
+}
